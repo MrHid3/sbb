e30299aed5e63fc44f4ab04de286c71619c96f7d
MrHid3 <gkornas@proton.me>
2025-04-26 23:19:04 +0200

    wysyłanie do zarejestrowanych socketów

--- App.js
+++ App.js
@@ -18,20 +18,52 @@ App.use(express.static(path.join(__dirname, 'public')));
 
 let users = [];
 
+function cleanSocket(socket){
+    for(let i = 0; i < users.length; i++){
+        if(users[i].socket == socket)
+            users.splice(i)
+    }
+}
+
+function socketToName(socket){
+    for(let i = 0; i < users.length; i++){
+        if(users[i].socket == socket)
+            return users[i].name;
+    }
+    return false;
+}
+
+function nameToSocket(name){
+    for(let i = 0; i < users.length; i++)
+        if(users[i].name == name)
+            return users[i].socket;
+    return false;
+}
+
 Io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on("authentication", name => {
+        cleanSocket(socket.id);
+        users.push({socket: socket.id, name: name});
+        console.log(users);
+    })
+
+    socket.on('message', data => {
+        console.log(data);
+        let socketID = nameToSocket(data.sendTo);
+        console.log(socketID);
+        if(socketID){
+            Io.to(socketID).emit('message', {name: socketToName(socket.id), message: data.message});
+            console.log({name: socketToName(socket.id), message: data.message});
+        }
+    })
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-    socket.on('message', (msg) => {
-        Io.emit('message', msg);
-    })
 });
 
-Io.on("authentication", name => {
-    console.log(name);
-    console.log("didi")
-})
+
 
 App.get("/", function(req, res) {
     res.render("index");
