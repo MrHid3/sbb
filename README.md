22a27e8ce57704a98aa9b0d5b27d0e8b7f828aca
MrHid3 <gkornas@proton.me>
2025-05-10 15:54:24 +0200

    literally updated README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # What if Signal was bad?
+
+In time, this will hopefully become a clone of Signal.
+
+### TO-DO
+- [ ] actually encrypt the database
+- [ ] session cookies
+- [ ] implement Diffie-Helman
+- [ ] adding friends
+- [ ] implement Double Ratchet
+- [ ] message storing
+- [ ] read markings
+
+### SIDE-QUESTS
+by how likely/easy they are to be implemented
+- [ ] profile pictures
+- [ ] sending images
+- [ ] someone is writing bubbles
+- [ ] group chats
+
+### MAYBE ONE DAY
+- [ ] voice calls
--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const hbs = require('express-handlebars');
 const formidable = require('formidable');
 const { Server } = require('socket.io');
 const cookieParser = require('cookie-parser');
-const App = express();
-const server = createServer(App)
-const Io = new Server(server)
+const app = express();
+const server = createServer(app)
+const io = new Server(server)
 const pool = require("./db")
 
-App.use(cookieParser());
-App.use(express.json());
+app.use(cookieParser());
+app.use(express.json());
 
-App.set('views', path.join(__dirname, 'views'));
-App.set('view engine', 'hbs');
-App.engine('hbs', hbs.engine({ defaultLayout: 'layout.hbs' }));
-App.use(express.json());
-App.use(express.urlencoded({ extended: false }));
-App.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({ defaultLayout: 'layout.hbs' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 let users = [];
 
-function cleanSocket(socket){
+function cleanSocket(socket){  //if a socket was previously assigned to a user, remove it
     for(let i = 0; i < users.length; i++){
         if(users[i].socket == socket)
             users.splice(i)
@@ -44,7 +44,7 @@ function idToSocket(id){
     return false;
 }
 
-Io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on("authentication", id => {
         cleanSocket(socket.id);
@@ -56,7 +56,7 @@ Io.on('connection', (socket) => {
         let socketID = idToSocket(data.sendTo);
         if(socketID){
             const senderId= socketToId(socket.id);
-            Io.to(socketID).emit('message', {name: senderId, message: data.message});
+            io.to(socketID).emit('message', {name: senderId, message: data.message});
             await pool.query("INSERT INTO MESSAGES(senderId, receiverId, message) values($1, $2, $3)", [senderId, data.sendTo, data.message])
         }
     })
@@ -67,19 +67,19 @@ Io.on('connection', (socket) => {
     });
 });
 
-App.get("/", async function(req, res) {
+app.get("/", async function(req, res) {
     if(!req.cookies.id)
         res.redirect(`/login`);
     res.render("index");
 });
 
-App.get("/login", function(req, res) {
+app.get("/login", function(req, res) {
     if(req.cookies.id)
         res.redirect("/");
     res.render("login");
 })
 
-App.post("/login", async function(req, res) {
+app.post("/login", async function(req, res) {
     const user = await pool.query("SELECT * FROM users where username = $1", [req.body.username]);
     if(user.rows.length != 1){
         res.send("WRONGCREDENTIALS");
@@ -89,7 +89,7 @@ App.post("/login", async function(req, res) {
     res.send("USERREGISTERED");
 })
 
-App.get("/register", function(req, res) {
+app.get("/register", function(req, res) {
     if(req.cookies.id){
         res.redirect("/");
         return
@@ -97,7 +97,7 @@ App.get("/register", function(req, res) {
     res.render("register");
 })
 
-App.post("/register", async function(req, res) {
+app.post("/register", async function(req, res) {
     const users = await pool.query("SELECT * FROM users where username = $1", [req.body.username]);
     if(users.rows.length > 0){
         res.send("USEREXISTS");
@@ -109,7 +109,7 @@ App.post("/register", async function(req, res) {
     res.send("USERREGISTERED");
 })
 
-App.get("/logout", function(req, res) {
+app.get("/logout", function(req, res) {
     res.clearCookie("id");
     res.redirect("/");
 })
